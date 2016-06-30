033b8c0c55dd9a45c226552494bc0480fefd3abc
buildcomplete <>
2016-06-30 19:06:18 -0400

    Updated code to record parth of the request path. this at least smells like something...

--- addbotsrc.go
+++ addbotsrc.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
+var uMap map[string]int
+
+func push(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Write([]byte("hello world"))
+		return
+	}
+		
+	add := r.URL.Path[1:]
+	uMap[add]= uMap[add] + 1
+
+	fmt.Println("U", r.Header["U"])
+
+	w.Header().Set("Content-Type", "application/json")
+	byt, _ := json.Marshal(uMap)
+	w.Write(byt)
+}
+
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	uMap = make(map[string]int)
+
+	http.HandleFunc("/p/", push)
+
+	http.ListenAndServe(":8080", nil)
+	
 }
