8681c11b10d1c2432f4eb9d470bf130a368d8696
buildcomplete <>
2016-07-02 13:47:24 -0400

    finished first version of a working principle

--- addbotsrc.go
+++ addbotsrc.go
@@ -2,34 +2,77 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"encoding/json"
+	"encoding/base64"
+	"strings"
 )
 
+// global 'Database' of URL's
 var uMap map[string]int
 
-func push(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Write([]byte("hello world"))
+// number of times to return an url after each hit
+var increment = 10
+
+// number of urls to transmit in each retr
+var sendcount = 20
+
+func vers(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("1"))
+}
+
+func retr(w http.ResponseWriter, r *http.Request) {
+	if len(uMap) == 0 {
+		w.WriteHeader(http.StatusNoContent)
 		return
 	}
+
+	i := sendcount
+	for k,v := range uMap {
+		w.Write([]byte(k))
+		w.Write([]byte("\n"))
 		
-	add := r.URL.Path[1:]
-	uMap[add]= uMap[add] + 1
+		if (v == 1) {
+			delete(uMap,k)
+			fmt.Println(len(uMap))
+		} else {
+			uMap[k]=v-1
+		}
 
-	fmt.Println("U", r.Header["U"])
+		i--
+		if i==0 {
+			return
+		}
+	}
+}
 
-	w.Header().Set("Content-Type", "application/json")
-	byt, _ := json.Marshal(uMap)
-	w.Write(byt)
+func push(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		
+		// U is a commasepperated list contaning the urls base64 encoded
+		// extract add add to uMap
+		data := r.Header["U"][0]
+		for _, f := range strings.Fields(data) {
+			if decoded, errD := base64.StdEncoding.DecodeString(f); errD == nil {
+				decodedStr := string(decoded)
+				uMap[decodedStr] = uMap[decodedStr] + increment
+			} else {
+				fmt.Print(errD)
+			}
+		}
+		
+		w.WriteHeader(http.StatusNoContent)
+		fmt.Println(len(uMap))
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 
 func main() {
 	uMap = make(map[string]int)
-
-	http.HandleFunc("/p/", push)
-
-	http.ListenAndServe(":8080", nil)
-	
+	http.HandleFunc("/v", vers)
+	http.HandleFunc("/p", push)
+	http.HandleFunc("/r", retr)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
